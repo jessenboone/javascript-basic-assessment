2c6c765553156c1c00e4f47333499ea91bb082ec
jessenboone <jessenboone@gmail.com>
2017-06-14 13:48:45 -0600

    Master fixed

--- assessment.js
+++ assessment.js
@@ -25,16 +25,19 @@ var myShirt = {
 };
 
 
+
 // Change my shirt type to spandex using square bracket notation
+var myOtherShirt = {
+	type: 'polo',
+	color: 'red'
+};
+
 
 
 // #5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
 // The key should be the animal name(string) and the value should be how many there are.
 // Our zoo has 8 monkeys, 4 giraffes and 2 elephants
-var myOtherShirt = {
-	type: 'polo',
-	color: 'red'
-};
+
 
 
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
@@ -49,12 +52,11 @@ var snake = {
 
 
 
-
 //#7 Create an array of strings that are the 7 primary colors in the rainbow -
 // red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
 
 // #8 Using this array do the following
-var heroes = ['superman', 'batman', 'flash'];
+
 // add 'wonderwoman' to the end
 
 // remove 'superman' and store him in a variable called firstHero
@@ -72,16 +74,14 @@ var heroes = ['superman', 'batman', 'flash'];
 // the array, and returns the array with the added item.
 
 
+
 // #10 Write a function called removeItem that takes in an array of strings, and a string.
 // Removes all instances of that string from the array. And return the modified array.
 // The order of the array should not be changed
 
 
-
-
 // #11 Write a function  called doubleTheFun that takes 1 parameter. It should double numbers, and
 // repeats strings. example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
-
 
 
 
@@ -97,6 +97,7 @@ var heroes = ['superman', 'batman', 'flash'];
 // the current time as a Date object
 
 
+
 // #14 Create a function called coderTest that takes in an object that is a person. It looks to see if the person’s
 // name is Jeremy and then changes the person object to have a property called lovesCode with a value of 10.  If their
 // name is Brack set lovesCode to 0.  otherwise set lovesCode to 5.
@@ -108,11 +109,17 @@ var heroes = ['superman', 'batman', 'flash'];
 
 /*
     temperature over 80 and humidity over 40 - return "I'm all sweat"
+
     temperature under 40 and cloudiness over 60 - return "I have icecicles"
+
     temperature over 80 and humidity under 40 and cloudiness under 20 - return "I'm literally in the desert"
+
     temperature over 80 or humidity over 50 or cloudiness over 50 - return "Hmm, probably not"
+
     Otherwise - return "I love outside"
 */
+
+
 
 
 
